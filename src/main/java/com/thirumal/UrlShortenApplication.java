53c74d1c21d872721ff7a1d0d0b07e823aec87ce
Thirumal <m.thirumal@hotmail.com>
2023-02-15 08:44:02 +0530

    Initial commit

---
+++ src/main/java/com/thirumal/UrlShortenApplication.java
@@ -0,0 +1,13 @@
+package com.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thirumal/UrlShortenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirumal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
