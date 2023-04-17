73abfdc6a6e799154795ef9f417076736dab026a
Thirumal <m.thirumal@hotmail.com>
2023-04-17 15:43:33 +0530

    data models are added

--- url_shorten.sql
+++ url_shorten.sql
@@ -1,5 +1,5 @@
 -- Database generated with pgModeler (PostgreSQL Database Modeler).
--- pgModeler version: 1.0.0
+-- pgModeler version: 1.0.1
 -- PostgreSQL version: 15.0
 -- Project Site: pgmodeler.io
 -- Model Author: Thirumal
@@ -57,6 +57,34 @@ CREATE INDEX ix_shorten_url_short_url ON public.shorten_url
 USING btree
 (
 	short_url
+);
+-- ddl-end --
+
+-- object: public.click | type: TABLE --
+-- DROP TABLE IF EXISTS public.click CASCADE;
+CREATE TABLE public.click (
+	click_id bigserial NOT NULL,
+	shorten_url_id bigint NOT NULL,
+	row_created_on timestamptz NOT NULL DEFAULT current_timestamp,
+	CONSTRAINT click_pk PRIMARY KEY (click_id)
+);
+-- ddl-end --
+ALTER TABLE public.click OWNER TO postgres;
+-- ddl-end --
+
+-- object: shorten_url_fk | type: CONSTRAINT --
+-- ALTER TABLE public.click DROP CONSTRAINT IF EXISTS shorten_url_fk CASCADE;
+ALTER TABLE public.click ADD CONSTRAINT shorten_url_fk FOREIGN KEY (shorten_url_id)
+REFERENCES public.shorten_url (shorten_url_id) MATCH FULL
+ON DELETE RESTRICT ON UPDATE CASCADE;
+-- ddl-end --
+
+-- object: ixfk_click_shorten_url_id | type: INDEX --
+-- DROP INDEX IF EXISTS public.ixfk_click_shorten_url_id CASCADE;
+CREATE INDEX ixfk_click_shorten_url_id ON public.click
+USING btree
+(
+	shorten_url_id
 );
 -- ddl-end --
 
